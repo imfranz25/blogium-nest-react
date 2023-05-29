d72ef6e9f21a5221dd279f1f630b06e00304b9e9
Francis Ong <francis.ong@stratpoint.com>
2023-05-29 23:12:45 +0800

    feat: add create user func - use bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { PrismaClientValidationError } from '@prisma/client/runtime';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  async create(createUserDto: CreateUserDto) {
+    const { password, confirmPassword, ...newUser } = createUserDto;
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+    if (password !== confirmPassword) {
+      throw new PrismaClientValidationError();
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    return this.prisma.user.create({
+      data: { ...newUser, hashedPassword },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
 }
+
+// findAll() {
+//   return `This action returns all user`;
+// }
+
+// findOne(id: number) {
+//   return `This action returns a #${id} user`;
+// }
+
+// remove(id: number) {
+//   return `This action removes a #${id} user`;
+// }
