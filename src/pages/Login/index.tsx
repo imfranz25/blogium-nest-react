db743ab219a0a45f517968fa7baf8344078913fb
Francis Ong <francis.ong@stratpoint.com>
2023-05-31 14:59:38 +0800

    feat: add login page and use input

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,29 @@
+import { useCallback } from 'react';
+import { useForm, FieldValues } from 'react-hook-form';
+
+import Input from '../../components/Input';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({ defaultValues: { email: '', password: '' } });
+
+  const onLogin = useCallback(() => {
+    alert('Login in submitted');
+    reset();
+  }, [reset]);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onLogin)}>
+        <Input label="Email" id="email" register={register} errors={errors} />
+        <Input label="Password" id="password" type="password" register={register} errors={errors} />
+      </form>
+    </>
+  );
 };
 
 export default LoginPage;
