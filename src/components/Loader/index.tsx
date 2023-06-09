b2412587721a101c8ba84b44d438d1d4511b133e
Francis Ong <francis.ong@stratpoint.com>
2023-06-09 08:08:30 +0800

    feat: change icon to puff loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,10 +1,10 @@
-import { BounceLoader } from 'react-spinners';
+import { PuffLoader } from 'react-spinners';
 import { LoaderWrapper } from './style';
 
 const Loader = () => {
   return (
     <LoaderWrapper>
-      <BounceLoader color="#87ceeb" size={100} aria-label="Loading posts" />
+      <PuffLoader color="#87ceeb" size={100} aria-label="Loading posts" />
     </LoaderWrapper>
   );
 };
