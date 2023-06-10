7d16a15e3a6f3591695597f6613f7d8d2632865c
Francis Ong <francis.ong@stratpoint.com>
2023-06-10 20:26:03 +0800

    feat: apply useFetch to login endpoint

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { toast } from 'react-hot-toast';
 import { useState, useEffect, useCallback } from 'react';
 
@@ -8,32 +8,44 @@ import useAuth from './useAuth';
 import { SafeError } from '../types';
 import getErrorMessage from '../utils/getErrorMessage';
 
-const useFetch = (endpoint: string, options: any) => {
+interface IUseFetch {
+  endpoint: string;
+  skipInitialInvocation?: boolean;
+}
+
+const useFetch = ({ endpoint, skipInitialInvocation = false }: IUseFetch) => {
   const API = import.meta.env.VITE_BACKEND_URL;
   const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<AxiosResponse | null>(null);
   const { sessionGuard } = useAuth();
 
-  const fetchData = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      sessionGuard();
+  const fetchData = useCallback(
+    async (config: AxiosRequestConfig = {}) => {
+      try {
+        setIsLoading(true);
+        sessionGuard();
 
-      const { data } = await axios(`${API}${endpoint}`, options);
+        const response = await axios(`${API}${endpoint}`, config);
 
-      setData(data);
-    } catch (error) {
-      toast.error(getErrorMessage(error as SafeError));
-    } finally {
-      setIsLoading(false);
-    }
-  }, [endpoint, options, API, sessionGuard]);
+        setData(response);
+
+        return response;
+      } catch (error) {
+        toast.error(getErrorMessage(error as SafeError));
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [endpoint, API, sessionGuard]
+  );
 
   useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    if (!skipInitialInvocation) {
+      fetchData();
+    }
+  }, [fetchData, skipInitialInvocation]);
 
-  return { isLoading, data };
+  return { isLoading, data, refetch: fetchData };
 };
 
 export default useFetch;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,38 +1,34 @@
-import * as api from '../../api';
 import { toast } from 'react-hot-toast';
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Button, Form, Row, Typography } from 'antd';
 import { AiOutlineLogin } from 'react-icons/ai';
 
-import getErrorMessage from '../../utils/getErrorMessage';
 import Input from '../../components/Input';
+import useAuth from '../../hooks/useAuth';
+import useFetch from '../../hooks/useFetch';
 import { LoginWrapper, LoginCard, ActionWrapper } from './styles';
-import { SafeError } from '../../types';
 import { Credentials } from '../../types/formTypes';
-import useUser from '../../hooks/useAuth';
 
 const LoginPage = () => {
-  const { registerSession } = useUser();
+  const { registerSession } = useAuth();
   const navigate = useNavigate();
-  const [isLoading, isSetLoading] = useState(false);
+  const { isLoading, refetch: loginUser } = useFetch({
+    endpoint: '/auth/login',
+    skipInitialInvocation: true,
+  });
 
   const onLogin = useCallback(
     async (credentials: Credentials) => {
-      try {
-        isSetLoading(true);
-        const { data } = await api.loginUser(credentials);
+      const response = await loginUser({ method: 'POST', data: credentials });
 
+      if (response) {
         toast.success('Logged in');
-        registerSession(data.accessToken);
+        registerSession(response.data.accessToken);
         navigate('/feed');
-      } catch (error) {
-        toast.error(getErrorMessage(error as SafeError));
-      } finally {
-        isSetLoading(false);
       }
     },
-    [navigate, registerSession]
+    [navigate, registerSession, loginUser]
   );
 
   return (
@@ -41,7 +37,6 @@ const LoginPage = () => {
         <Row justify="center">
           <Typography.Title level={3}>Welcome back!</Typography.Title>
         </Row>
-
         <Form onFinish={onLogin}>
           <Input label="Email" type="email" id="email" required />
           <Input label="Password" id="password" type="password" required />
@@ -57,7 +52,6 @@ const LoginPage = () => {
             </Button>
           </ActionWrapper>
         </Form>
-
         <Row justify="center">
           <Typography.Paragraph>
             Don&apos;t have an account? <Link to="/sign-up">Sign up</Link>
