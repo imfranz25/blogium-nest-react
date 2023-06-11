9cdde95047c71f8a35b98f622fe9466550306719
Francis Ong <francis.ong@stratpoint.com>
2023-06-12 00:11:01 +0800

    feat: apply useFetch in fetching user details

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,55 +1,31 @@
-import { useCallback, useEffect, useState } from 'react';
-import { toast } from 'react-hot-toast';
+import { Col, Row, Typography } from 'antd';
 import { useParams } from 'react-router-dom';
-import * as api from '../../api';
 
-import getErrorMessage from '../../utils/getErrorMessage';
-import useAuth from '../../hooks/useAuth';
-import { SafePostUser } from '../../types';
+import useFetch from '../../hooks/useFetch';
 import Loader from '../../components/Loader';
-import { Typography } from 'antd';
 
 const ProfilePage = () => {
-  const { token } = useAuth();
   const params = useParams();
-  const [isLoading, setIsLoading] = useState(false);
-  const [userData, setUserData] = useState<SafePostUser>();
-
-  const fetchUser = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const response = await api.getUser(params.id ?? '', token);
-
-      setUserData(response.data);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      if (error?.response?.status !== 404) {
-        toast.error(getErrorMessage(error));
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  }, [params.id, token]);
-
-  useEffect(() => {
-    fetchUser();
-  }, [fetchUser]);
+  const { isLoading, resData } = useFetch({ endpoint: `/user/${params.id}` });
+  const userData = resData?.data;
 
   if (isLoading) {
     return <Loader />;
   }
 
   return (
-    <>
-      {userData ? (
-        <>
-          <div>Profile Picture: {userData?.profilePicture}</div>
-          <div>Name: {userData?.firstName}</div>
-        </>
-      ) : (
-        <Typography.Title>User not found</Typography.Title>
-      )}
-    </>
+    <Row justify="space-around">
+      <Col span={16}>
+        {userData ? (
+          <>
+            <div>Profile Picture: {userData?.profilePicture}</div>
+            <div>Name: {userData?.firstName}</div>
+          </>
+        ) : (
+          <Typography.Title>User not found</Typography.Title>
+        )}
+      </Col>
+    </Row>
   );
 };
 
